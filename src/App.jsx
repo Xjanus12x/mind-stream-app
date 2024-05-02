9dd195ea3d64bdd1c63cfbccbf012ba3902fcb2b
xjanus12x <albertovillacarlos07@gmail.com>
2024-05-02 19:39:14 +0800

    Navbar and PostCard Components used

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import "./App.css";
+import PostCard from "./components/PostCard/PostCard";
+import Navbar from "./components/navbar/Navbar";
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <header>
+        <Navbar name="Alice" />
+        <h2 className="font-bold text-center uppercase text-7xl leading-[5.446rem] border-t border-b border-black">
+          The Blog
+        </h2>
+      </header>
+      <main>
+        <section className="container p-8 space-y-8">
+          <header>
+            <h2 className="text-2xl font-semibold leading-8 ">
+              Recent blog posts
+            </h2>
+          </header>
+          <article className="space-y-6">
+            <img src="src/assets/icons/blog-post-1.png" alt="Blog post" />
+            <div className="space-y-6">
+              <p>Sunday , 1 Jan 2023</p>
+              <div className="flex justify-between">
+                <h3 className="text-2xl font-semibold">
+                  UX review presentations
+                </h3>
+                <img src="src/assets/icons/visit-icon.png" alt="Arrow icon" />
+              </div>
+              <p>
+                How do you create compelling presentations that wow your
+                colleagues and impress your managers?
+              </p>
+              <div className="space-x-2">
+                <button>Design</button>
+                <button>Research</button>
+                <button>Presentation</button>
+              </div>
+            </div>
+          </article>
+
+          <PostCard />
+        </section>
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
