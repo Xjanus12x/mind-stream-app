144b75b88f1ac292afee627784caa1b5c6088102
xjanus12x <albertovillacarlos07@gmail.com>
2024-05-12 22:27:34 +0800

    Use PostCard,Navbar,Footer  components

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import PostCard from "./components/PostCard/PostCard";
-import Navbar from "./components/navbar/Navbar";
+import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
 function App() {
   return (
     <>
@@ -17,31 +18,165 @@ function App() {
               Recent blog posts
             </h2>
           </header>
-          <article className="space-y-6">
-            <img src="src/assets/icons/blog-post-1.png" alt="Blog post" />
-            <div className="space-y-6">
-              <p>Sunday , 1 Jan 2023</p>
-              <div className="flex justify-between">
-                <h3 className="text-2xl font-semibold">
-                  UX review presentations
-                </h3>
-                <img src="src/assets/icons/visit-icon.png" alt="Arrow icon" />
-              </div>
-              <p>
-                How do you create compelling presentations that wow your
-                colleagues and impress your managers?
-              </p>
-              <div className="space-x-2">
-                <button>Design</button>
-                <button>Research</button>
-                <button>Presentation</button>
-              </div>
-            </div>
-          </article>
 
-          <PostCard />
+          <section className="grid gap-8">
+            <PostCard
+              date={new Date()}
+              title="UX review presentations"
+              postImg="src/assets/icons/blog-post-1.png"
+              description="How do you create compelling presentations that wow your colleagues and impress your managers?"
+              categories={[
+                {
+                  category_title: "Design",
+                  text_color: "text-purple-600",
+                  bg_color: "bg-purple-100",
+                },
+                {
+                  category_title: "Research",
+                  text_color: "text-blue-600",
+                  bg_color: "bg-blue-100",
+                },
+                {
+                  category_title: "Presentation",
+                  text_color: "text-pink-600",
+                  bg_color: "bg-pink-100",
+                },
+              ]}
+            />
+
+            <PostCard
+              date={new Date()}
+              title="UX review presentations"
+              postImg="src/assets/icons/blog-post-1.png"
+              description="How do you create compelling presentations that wow your colleagues and impress your managers?"
+              categories={[
+                {
+                  category_title: "Design",
+                  text_color: "text-purple-600",
+                  bg_color: "bg-purple-100",
+                },
+                {
+                  category_title: "Research",
+                  text_color: "text-blue-600",
+                  bg_color: "bg-blue-100",
+                },
+                {
+                  category_title: "Presentation",
+                  text_color: "text-pink-600",
+                  bg_color: "bg-pink-100",
+                },
+              ]}
+            />
+
+            <PostCard
+              date={new Date()}
+              title="UX review presentations"
+              postImg="src/assets/icons/blog-post-1.png"
+              description="How do you create compelling presentations that wow your colleagues and impress your managers?"
+              categories={[
+                {
+                  category_title: "Design",
+                  text_color: "text-purple-600",
+                  bg_color: "bg-purple-100",
+                },
+                {
+                  category_title: "Research",
+                  text_color: "text-blue-600",
+                  bg_color: "bg-blue-100",
+                },
+                {
+                  category_title: "Presentation",
+                  text_color: "text-pink-600",
+                  bg_color: "bg-pink-100",
+                },
+              ]}
+            />
+          </section>
+        </section>
+
+        <section className="container p-8 space-y-8 mx-auto ">
+          <header className="">
+            <h2 className="text-2xl font-semibold leading-8 ">
+              All blog posts
+            </h2>
+          </header>
+          <section className="grid gap-8 sm:grid-cols-2 lg:grid-cols-3 justify-items-center items-start">
+            <PostCard
+              date={new Date()}
+              title="UX review presentations"
+              postImg="src/assets/icons/blog-post-1.png"
+              description="How do you create compelling presentations that wow your colleagues and impress your managers?"
+              categories={[
+                {
+                  category_title: "Design",
+                  text_color: "text-purple-600",
+                  bg_color: "bg-purple-100",
+                },
+                {
+                  category_title: "Research",
+                  text_color: "text-blue-600",
+                  bg_color: "bg-blue-100",
+                },
+                {
+                  category_title: "Presentation",
+                  text_color: "text-pink-600",
+                  bg_color: "bg-pink-100",
+                },
+              ]}
+            />
+
+            <PostCard
+              date={new Date()}
+              title="UX review presentations"
+              postImg="src/assets/icons/blog-post-1.png"
+              description="How do you create compelling presentations that wow your colleagues and impress your managers?"
+              categories={[
+                {
+                  category_title: "Design",
+                  text_color: "text-purple-600",
+                  bg_color: "bg-purple-100",
+                },
+                {
+                  category_title: "Research",
+                  text_color: "text-blue-600",
+                  bg_color: "bg-blue-100",
+                },
+                {
+                  category_title: "Presentation",
+                  text_color: "text-pink-600",
+                  bg_color: "bg-pink-100",
+                },
+              ]}
+            />
+
+            <PostCard
+              date={new Date()}
+              title="UX review presentations"
+              postImg="src/assets/icons/blog-post-1.png"
+              description="How do you create compelling presentations that wow your colleagues and impress your managers?"
+              categories={[
+                {
+                  category_title: "Design",
+                  text_color: "text-purple-600",
+                  bg_color: "bg-purple-100",
+                },
+                {
+                  category_title: "Research",
+                  text_color: "text-blue-600",
+                  bg_color: "bg-blue-100",
+                },
+                {
+                  category_title: "Presentation",
+                  text_color: "text-pink-600",
+                  bg_color: "bg-pink-100",
+                },
+              ]}
+            />
+          </section>
         </section>
       </main>
+
+      <Footer />
     </>
   );
 }
