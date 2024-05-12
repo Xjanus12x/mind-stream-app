11996e3a56af80790bdfee414c322b394ac09b85
xjanus12x <albertovillacarlos07@gmail.com>
2024-05-12 22:26:51 +0800

    Added PostCard component

--- src/components/PostCard/PostCard.jsx
+++ src/components/PostCard/PostCard.jsx
@@ -1,33 +1,33 @@
 import React from "react";
 
 function PostCard(props) {
-  const { title } = props;
-  const test = [
-    {
-      category_title: "test1",
-      text_color: "text-white",
-      bg_color: "bg-blue-500",
-    },
-    {
-      category_title: "test2",
-      text_color: "text-white",
-      bg_color: "bg-green-500",
-    },
-  ];
+  const { title, postImg, date, description, categories } = props;
+  // Convert date to a JavaScript Date object
+  const postDate = new Date(date);
+  // Format the date to display in the desired format
+  const formattedDate = postDate.toLocaleDateString("en-US", {
+    weekday: "long", // Full name of the day of the week
+    day: "numeric", // Day of the month
+    month: "short", // Abbreviated month name
+    year: "numeric", // Full year
+  });
+
   return (
-    <article className="space-y-6">
-      <img src="src/assets/icons/blog-post-1.png" alt="Blog post" />
+    <article className="space-y-6 w-full">
+      <img className="w-full object-cover" src={postImg} alt="Blog post" />
       <div className="space-y-6">
-        <p>Sunday , 1 Jan 2023</p>
-        <div className="flex justify-between">
-          <h3 className="text-2xl font-semibold">{props.title}</h3>
-          <img src="src/assets/icons/visit-icon.png" alt="Arrow icon" />
+        <p className="font-semibold text-purple-800 text-sm">{formattedDate}</p>
+        <div className="flex justify-between items-center flex-wrap">
+          <h3 className="text-2xl font-semibold">{title}</h3>
+          <span>
+            <img src="src/assets/icons/visit-icon.png" alt="Arrow icon" />
+          </span>
         </div>
-        <p>{props.description}</p>
-        <div className="space-x-2">
-          {test.map((item, index) => (
+        <p className="text-gray-600 leading-6 max-w-[40ch]">{description}</p>
+        <div className="flex gap-2 flex-wrap">
+          {categories.map((item, index) => (
             <button
-              className={`${item.text_color} ${item.bg_color}`}
+              className={`${item.text_color} ${item.bg_color} py-[2px] px-[10px] rounded-full font-medium`}
               key={index}
             >
               {item.category_title}
